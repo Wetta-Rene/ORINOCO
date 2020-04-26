dad7da750f8d6051dd9e3b03e313b77b78e527db
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-26 21:07:45 +0200

    Début du projet avec page catalogue ok

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+var functionOpenLink = function (a) {
+    window.open('http://localhost:3000/api/' + a, '_blank');
+}
+
+
+/*requette XML vers le backend*/
+function showCustomer(catalogue) {
+    /*nettoyage du div en cas de remplissage avant*/
+    document.getElementById("resultats").innerHTML = "";
+    var xhttp;
+    if (catalogue == "") {
+        document.getElementById("resultats").innerHTML = "";
+        return;
+    }
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            var div = document.getElementById("resultats");
+            var table = document.createElement("table");
+            var tableBody = document.createElement("tbody");
+            var tableResultats = document.getElementById("resultats");
+
+            for (var i = 0; i < response.length; i++){
+                var objet = response[i];
+                
+                var row = '<article class="containerArticle"><div class="elementContainerArticle"><img src="'+ objet.imageUrl +'" ></div><div class="elementContainerArticle"><div class="contentDescriptionArticle"><h2>'+ objet.name +'</h2><div class="textDescription">'+ objet.description +'</div><div class="lienPageCustomArticle"><a href="produit.html?catalogue='+catalogue+'&produit='+[i]+'"> >> Personnalisé ce produit</a></div></div></div></article>';
+
+                /*affichage des differents article*/
+                tableResultats.innerHTML += row;      
+
+            }
+            
+        }//fin du if readyState
+    };
+    xhttp.open("GET", "http://localhost:3000/api/" + catalogue, true);
+    xhttp.send();
+}
