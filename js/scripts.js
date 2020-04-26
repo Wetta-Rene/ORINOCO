e49a97dde4cbd62105e1736a576d71800fdb6064
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-26 22:21:34 +0200

    no message

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var functionOpenLink = function (a) {
 }
 
 
-/*requette XML vers le backend*/
+/*requette XML vers le backend pour la page index.html */
 function showCustomer(catalogue) {
     /*nettoyage du div en cas de remplissage avant*/
     document.getElementById("resultats").innerHTML = "";
@@ -16,18 +16,16 @@ function showCustomer(catalogue) {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(this.responseText);
-            var div = document.getElementById("resultats");
-            var table = document.createElement("table");
-            var tableBody = document.createElement("tbody");
-            var tableResultats = document.getElementById("resultats");
+
+            var Resultats = document.getElementById("resultats");
 
             for (var i = 0; i < response.length; i++){
                 var objet = response[i];
                 
-                var row = '<article class="containerArticle"><div class="elementContainerArticle"><img src="'+ objet.imageUrl +'" ></div><div class="elementContainerArticle"><div class="contentDescriptionArticle"><h2>'+ objet.name +'</h2><div class="textDescription">'+ objet.description +'</div><div class="lienPageCustomArticle"><a href="produit.html?catalogue='+catalogue+'&produit='+[i]+'"> >> Personnalisé ce produit</a></div></div></div></article>';
+                var article = '<article class="containerArticle"><div class="elementContainerArticle"><img src="'+ objet.imageUrl +'" ></div><div class="elementContainerArticle"><div class="contentDescriptionArticle"><h2>'+ objet.name +'</h2><div class="textDescription">'+ objet.description +'</div><div class="lienPageCustomArticle"><a href="produit.html?catalogue='+catalogue+'&produit='+[i]+'"> >> Personnalisé ce produit</a></div></div></div></article>';
 
                 /*affichage des differents article*/
-                tableResultats.innerHTML += row;      
+                Resultats.innerHTML += article;      
 
             }
             
@@ -36,3 +34,7 @@ function showCustomer(catalogue) {
     xhttp.open("GET", "http://localhost:3000/api/" + catalogue, true);
     xhttp.send();
 }
+
+
+
+/*requette XML vers le backend pour la page produit.html (index) */
