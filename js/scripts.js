213103078e2a83b038b1349330135ace0aae1608
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-04-30 22:47:48 +0200

    avant panier

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,15 @@ var functionOpenLink = function (a) {
     window.open('http://localhost:3000/api/' + a, '_blank');
 }
 
+//objet ligne panier
+var lignePanier = {
+  reference: String,
+  option: String,
+  qte: String,
+  prix: String,
+  catalogue: String
+}
+
 
 
 
