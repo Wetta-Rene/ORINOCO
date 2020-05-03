f1c5e0e3e72e7aaafe475f1f82890f1926724058
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-05-03 22:03:47 +0200

    no message

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,66 @@
-var functionOpenLink = function (a) {
-    window.open('http://localhost:3000/api/' + a, '_blank');
+
+/////////////////////fonction calcul total des articles du pannier////////////////////////////
+var functionCalculArticlesDuPanier = function(){
+var paniers = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
+var tableauDeContageArticle = [];
+            for(let x in paniers) {
+               var lignePanier = paniers[x] ;
+                tableauDeContageArticle.push(lignePanier.quantite);
+            }
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;// calcul sommes de chaque donnee du tableau
+    const totalDesArticlesDuPanier = tableauDeContageArticle.reduce(reducer);
+return totalDesArticlesDuPanier;
 }
 
+////////////////////////////////////////// END ///////////////////////////////////////////////
 
+/////////////////////fonction calcul total des sommes  du pannier////////////////////////////
+var functionCalculPrixTotalDuPanier = function(){
+var paniers = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
+var tableauDeContageDesPrix = [];
+            for(let x in paniers) {
+               var lignePanier = paniers[x] ;
+                tableauDeContageDesPrix.push(lignePanier.prixAjour);
+            }
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const prixTotalDuPanier = tableauDeContageDesPrix.reduce(reducer);
+return prixTotalDuPanier;
+}
+////////////////////////////////////////// END ///////////////////////////////////////////////
 
+/////////////////////fonction calcul total des sommes  du pannier////////////////////////////
 
+var fonctionActionPanier = function(a){
+var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
+ var produitTrouve = false;
+            for(let x in data) {
+                 if(data[x].reference == a){
+                     produitTrouve = true;
+
+                     // Augmenter la quantité et le prix
+                     data[x].quantite++;
+                     data[x].prixAjour = data[x].quantite * data[x].prixUnitaire;
+                 }
+            }
+            // Sauvegarde du panier mis à jour
+            localStorage.setItem("panier", JSON.stringify(data));
+            window.location.href = "panier.html";// on revient à la page du panier   
+    
+}
+
+/*if(action == "modificationPlus"){ // modification quantite panier en plus
+var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
+ var produitTrouve = false;
+            for(let x in data) {
+                 if(data[x].reference == Id){
+                     produitTrouve = true;
+
+                     // Augmenter la quantité et le prix
+                     data[x].quantite++;
+                     data[x].prixAjour = data[x].quantite * data[x].prixUnitaire;
+                 }
+            }
+            // Sauvegarde du panier mis à jour
+            localStorage.setItem("panier", JSON.stringify(data));
+            window.location.href = "panier.html";// on revient à la page du produit    
+}// fin de modificationPlus  
