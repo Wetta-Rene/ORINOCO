5d6a77d5abe89977693e23f173d29f4709e70c99
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-05-05 22:37:00 +0200

    no message

--- js/scripts.js
+++ js/scripts.js
@@ -6,44 +6,41 @@
 
 /////////////////////fonction calcul total des articles du pannier////////////////////////////
 var functionCalculArticlesDuPanier = function(){
-var paniers = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
-        var tableauDeContageArticle = [];
-                    for(let x in paniers) {
-                       var lignePanier = paniers[x] ;
-                        tableauDeContageArticle.push(lignePanier.quantite);
-                    }
-            const reducer = (accumulator, currentValue) => accumulator + currentValue;// calcul sommes de chaque donnee du tableau
-            const totalDesArticlesDuPanier = tableauDeContageArticle.reduce(reducer);
-        return totalDesArticlesDuPanier;
-        }
-
+    var paniers = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
+    var quantite = 0;
+    for(let x in paniers) {   
+        quantite += parseInt(paniers[x].quantite);
+    }
+    
+    return quantite;
+}
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 
 /////////////////////fonction calcul total des sommes  du pannier////////////////////////////
 var functionCalculPrixTotalDuPanier = function(){
 var paniers = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
 var tableauDeContageDesPrix = [];
-            for(let x in paniers) {
+        for(let x in paniers) {
                var lignePanier = paniers[x] ;
                 tableauDeContageDesPrix.push(lignePanier.prixAjour);
-            }
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    const prixTotalDuPanier = tableauDeContageDesPrix.reduce(reducer);
+        }
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        const prixTotalDuPanier = tableauDeContageDesPrix.reduce(reducer);
 return prixTotalDuPanier;
 }
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 
 /////////////////////fonction calcul total des sommes  du pannier////////////////////////////
 
-var fonctionActionPanier = function(a){
+var fonctionActionPanier = function(a,Number){
 var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
- var produitTrouve = false;
+var produitTrouve = false;
             for(let x in data) {
                  if(data[x].reference == a){
                      produitTrouve = true;
 
                      // Augmenter la quantité et le prix
-                     data[x].quantite++;
+                     data[x].quantite = data[x].quantite + Number;
                      data[x].prixAjour = data[x].quantite * data[x].prixUnitaire;
                  }
             }
@@ -52,27 +49,52 @@ var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier 
             window.location.href = "panier.html";// on revient à la page du panier   
     
 }
+
+function deletePanier(a){
+    let id = a;
+    var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
+    data.slice(a,1);
+    localStorage.setItem("panier", JSON.stringify(data));
+    
+    
+ }
+                             
+
+
+
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 
 ///////////////////////////////   Affichage à cote du bouton panier     //////////////////////////////////////////////
 var fonctionAffichageHeader = function (){
 let affichageCoteBoutonPanier = null;
-if(localStorage.getItem("panier") === "vide"){// panier vide
-     affichageCoteBoutonPanier = '<button class="bouton"><a href="index.html">Accueil</a></button><button class="bouton"><a href="panier.html" title="0.00€">Panier (0 article)</a></button>';  
- }else{
-      const totalDesArticlesDuPanier = functionCalculArticlesDuPanier();// on fait le calcul des articles
-      const prixTotalDuPanier = functionCalculPrixTotalDuPanier(); // on fait le calcul total des prix du panier
-       let plurielArticleBoutonPanier = "";
-        if(totalDesArticlesDuPanier > 1){// mettre S a article ou pas
-            plurielArticleBoutonPanier = "articles";
-        }else{
-            plurielArticleBoutonPanier = "article";
-        }
-      affichageCoteBoutonPanier = '<button><a href="index.html">Accueil</a></button><button><a href="panier.html" title="'+prixTotalDuPanier+'€">Panier ('+totalDesArticlesDuPanier+' '+plurielArticleBoutonPanier+')</a></button>'; 
- }
+    if(localStorage.getItem("panier") === "vide"){// panier vide
+         affichageCoteBoutonPanier = '<button class="bouton"><a href="index.html">Accueil</a></button><button class="bouton"><a href="panier.html" title="0.00€">Panier (0 article)</a></button>';  
+     }else{
+          const totalDesArticlesDuPanier = functionCalculArticlesDuPanier();// on fait le calcul des articles
+          const prixTotalDuPanier = functionCalculPrixTotalDuPanier(); // on fait le calcul total des prix du panier
+           let plurielArticleBoutonPanier = "";
+            if(totalDesArticlesDuPanier > 1){// mettre S a article ou pas
+                plurielArticleBoutonPanier = "articles";
+            }else{
+                plurielArticleBoutonPanier = "article";
+            }
+          affichageCoteBoutonPanier = '<button><a href="index.html">Accueil</a></button><button><a href="panier.html" title="'+prixTotalDuPanier+'€">Panier ('+totalDesArticlesDuPanier+' '+plurielArticleBoutonPanier+')</a></button>'; 
+     }
 return affichageCoteBoutonPanier;
 }
      
  //////////////////////////////////////////////  END    ////////////////////////////////////////////////////
  
+///////////////////////////////   AFFICHAGE EN EURO   ///////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////// END ///////////////////////////////////////////////
 
