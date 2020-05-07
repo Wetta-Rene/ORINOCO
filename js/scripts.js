ff11ff25f675ae95019f00ee6cb16aefb2d6fd0b
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-05-07 15:44:10 +0200

    no message

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ localStorage.setItem("messagePanier", "vide");
 
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 
-///////////////////// FONCTIN CALCUL DES ARTICLES DU PANIER ////////////////////////////
+///////////////////// FONCTIN CALCUL TOTAL DES ARTICLES DU PANIER ////////////////////////////
 var functionCalculArticlesDuPanier = function(){
     var paniers = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
     var quantite = 0;
@@ -18,41 +18,18 @@ var functionCalculArticlesDuPanier = function(){
 }
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 
-/////////////////////fonction calcul total des sommes  du pannier////////////////////////////
+///////////////////// FONCTION CALCUL TOTAL PRIX DU PANIER ////////////////////////////
 var functionCalculPrixTotalDuPanier = function(){
 var paniers = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
 var tableauDeContageDesPrix = [];
-        for(let x in paniers) {
-               var lignePanier = paniers[x] ;
-                tableauDeContageDesPrix.push(lignePanier.prixAjour);
-        }
-        const reducer = (accumulator, currentValue) => accumulator + currentValue;
-        const prixTotalDuPanier = tableauDeContageDesPrix.reduce(reducer);
-return prixTotalDuPanier;
-}
-////////////////////////////////////////// END ///////////////////////////////////////////////
-
-
-
-
-///////////////////// FONCTION CALCUL DES SOMMES DU PANIER  ////////////////////////////
-
-var fonctionActionPanier = function(a,Number){
-var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
-var produitTrouve = false;
-            for(let x in data) {
-                 if(data[x].reference == a){
-                     produitTrouve = true;
-
-                     // Augmenter la quantité et le prix
-                     data[x].quantite = data[x].quantite + Number;
-                     data[x].prixAjour = data[x].quantite * data[x].prixUnitaire;
-                 }
-            }
-            // Sauvegarde du panier mis à jour
-            localStorage.setItem("panier", JSON.stringify(data));
-            window.location.href = "panier.html";// on revient à la page du panier   
+    for(let x in paniers) {
+        var lignePanier = paniers[x] ;
+        tableauDeContageDesPrix.push(lignePanier.prixAjour);
+    }
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const prixTotalDuPanier = tableauDeContageDesPrix.reduce(reducer);
     
+return prixTotalDuPanier;
 }
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 
@@ -61,20 +38,22 @@ var produitTrouve = false;
 ///////////////////////////////   AFFICHAGE DU HEADER DES PAGES DU SITE    //////////////////////////////////////////////
 var fonctionAffichageHeader = function (){
 let affichageCoteBoutonPanier = null;
-    if(localStorage.getItem("panier") === "vide"){// panier vide
-         affichageCoteBoutonPanier = '<button class="bouton"><a href="index.html">Accueil / Catalogue</a></button><button class="bouton"><a href="panier.html" title="0.00€">Panier (0 article)</a></button>';  
-     }else{
-          const totalDesArticlesDuPanier = functionCalculArticlesDuPanier();// on fait le calcul des articles
-          const prixTotalDuPanier = functionCalculPrixTotalDuPanier(); // on fait le calcul total des prix du panier
-           let plurielArticleBoutonPanier = "";
-            if(totalDesArticlesDuPanier > 1){// mettre S a article ou pas
-                plurielArticleBoutonPanier = "articles";
-            }else{
-                plurielArticleBoutonPanier = "article";
-            }
-          affichageCoteBoutonPanier = '<button><a href="index.html">Accueil / Catalogue</a></button><button><a href="panier.html" title="'+prixTotalDuPanier+'€">Panier ('+totalDesArticlesDuPanier+' '+plurielArticleBoutonPanier+')</a></button>'; 
-     }
-return affichageCoteBoutonPanier;
+        if(localStorage.getItem("panier") === "vide"){// panier vide
+             affichageCoteBoutonPanier = '<button class="bouton"><a href="index.html">Accueil / Catalogue</a></button><button class="bouton"><a href="panier.html" title="0.00€">Panier (0 article)</a></button>';  
+         }else{
+            const totalDesArticlesDuPanier = functionCalculArticlesDuPanier();// on fait le calcul des articles
+            const prixTotalDuPanier = functionCalculPrixTotalDuPanier(); // on fait le calcul total des prix du panier
+            let plurielArticleBoutonPanier = "";
+               if(totalDesArticlesDuPanier > 1){// mettre S a article ou pas
+                    plurielArticleBoutonPanier = "articles";
+                }else{
+                    plurielArticleBoutonPanier = "article";
+                }
+
+        affichageCoteBoutonPanier = '<button><a href="index.html">Accueil / Catalogue</a></button><button><a href="panier.html" title="'+prixTotalDuPanier+'€">Panier ('+totalDesArticlesDuPanier+' '+plurielArticleBoutonPanier+')</a></button>'; 
+         }
+
+    return affichageCoteBoutonPanier;
 }
      
  //////////////////////////////////////////////  END    ////////////////////////////////////////////////////
@@ -92,13 +71,13 @@ return affichageCoteBoutonPanier;
 //////////////////////////////////////////////////////////////// GESTION DU PANIER /////////////////////////////////////////////////////////////////////
 
 
-//////////////////////////////////////////////////////////////// MESSAGE FLASH UTILISATEUR  //////////////////////////////////////////////////
+//////////////////////////////////////// MESSAGE FLASH UTILISATEUR  //////////////////////////////////////////////////
 var affichageMessageFlash = function(){
    let message = localStorage.messagePanier;
-    if(message != "vide"){
-    let affichageMessageFlash = '<div id="textFlashMessage">'+message+'</div>';
-    document.getElementById("flashMessage").innerHTML = '<div id="textFlashMessage">'+message+'</div>';
-    }
+        if(message != "vide"){
+        let affichageMessageFlash = '<div id="textFlashMessage">'+message+'</div>';
+        document.getElementById("flashMessage").innerHTML = '<div id="textFlashMessage">'+message+'</div>';
+        }
     localStorage.messagePanier = "vide"; // on remet vide en cas de rechargement de la page
 }
 
@@ -119,22 +98,23 @@ var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier 
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////// MODIFICATION QUANTITE ////////////////////////////////////////////
-let fonctionSetupPanier = function(a,b){  // moins ou plus article panier
+//////////////////////////// MODIFICATION QUANTITE ////////////////////////////////////////////
+
+var fonctionActionPanier = function(a,Number){
 var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
 var produitTrouve = false;
             for(let x in data) {
                  if(data[x].reference == a){
                      produitTrouve = true;
-
                      // Augmenter la quantité et le prix
-                     data[x].quantite += b;
+                     data[x].quantite = data[x].quantite + Number;
                      data[x].prixAjour = data[x].quantite * data[x].prixUnitaire;
                  }
             }
             // Sauvegarde du panier mis à jour
             localStorage.setItem("panier", JSON.stringify(data));
-    
-            window.location.href = "panier.html";// on revient à la page du produit  */ 
+            window.location.href = "panier.html";// on revient à la page du panier    
 }
+////////////////////////////////////////// END ///////////////////////////////////////////////
+
 //////////////////////////////////////////////////////////////// END GESTION DU PANIER /////////////////////////////////////////////////////////////////////
