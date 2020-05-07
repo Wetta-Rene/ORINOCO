29f1fabebbc4f958bed4333ed87a6a01ea2d41ce
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-05-07 16:39:57 +0200

    no message

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,63 @@
 /////////////////////////////// INITIALISATION D'UN PANIER    //////////////////////////////////////////////
 if (localStorage.getItem("panier") == null) {
-localStorage.setItem("panier", "vide");
-localStorage.setItem("messagePanier", "vide");
+    localStorage.setItem("panier", "vide");
+    localStorage.setItem("messagePanier", "vide");
 }
 
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 
 ///////////////////// FONCTIN CALCUL TOTAL DES ARTICLES DU PANIER ////////////////////////////
-var functionCalculArticlesDuPanier = function(){
+var functionCalculArticlesDuPanier = function () {
     var paniers = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
     var quantite = 0;
-    for(let x in paniers) {   
+    for (let x in paniers) {
         quantite += parseInt(paniers[x].quantite);
     }
-    
+
     return quantite;
 }
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 
 ///////////////////// FONCTION CALCUL TOTAL PRIX DU PANIER ////////////////////////////
-var functionCalculPrixTotalDuPanier = function(){
-var paniers = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
-var tableauDeContageDesPrix = [];
-    for(let x in paniers) {
-        var lignePanier = paniers[x] ;
+var functionCalculPrixTotalDuPanier = function () {
+    var paniers = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
+    var tableauDeContageDesPrix = [];
+    for (let x in paniers) {
+        var lignePanier = paniers[x];
         tableauDeContageDesPrix.push(lignePanier.prixAjour);
     }
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     const prixTotalDuPanier = tableauDeContageDesPrix.reduce(reducer);
-    
-return prixTotalDuPanier;
+
+    return prixTotalDuPanier;
 }
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 
 
 
 ///////////////////////////////   AFFICHAGE DU HEADER DES PAGES DU SITE    //////////////////////////////////////////////
-var fonctionAffichageHeader = function (){
-let affichageCoteBoutonPanier = null;
-        if(localStorage.getItem("panier") === "vide"){// panier vide
-             affichageCoteBoutonPanier = '<button class="bouton"><a href="index.html">Accueil / Catalogue</a></button><button class="bouton"><a href="panier.html" title="0.00€">Panier (0 article)</a></button>';  
-         }else{
-            const totalDesArticlesDuPanier = functionCalculArticlesDuPanier();// on fait le calcul des articles
-            const prixTotalDuPanier = functionCalculPrixTotalDuPanier(); // on fait le calcul total des prix du panier
-            let plurielArticleBoutonPanier = "";
-               if(totalDesArticlesDuPanier > 1){// mettre S a article ou pas
-                    plurielArticleBoutonPanier = "articles";
-                }else{
-                    plurielArticleBoutonPanier = "article";
-                }
+var fonctionAffichageHeader = function () {
+    let affichageCoteBoutonPanier = null;
+    if (localStorage.getItem("panier") === "vide") { // panier vide
+        affichageCoteBoutonPanier = '<button class="bouton"><a href="index.html">Accueil / Catalogue</a></button><button class="bouton"><a href="panier.html" title="0.00€">Panier (0 article)</a></button>';
+    } else {
+        const totalDesArticlesDuPanier = functionCalculArticlesDuPanier(); // on fait le calcul des articles
+        const prixTotalDuPanier = functionCalculPrixTotalDuPanier(); // on fait le calcul total des prix du panier
+        let plurielArticleBoutonPanier = "";
+        if (totalDesArticlesDuPanier > 1) { // mettre S a article ou pas
+            plurielArticleBoutonPanier = "articles";
+        } else {
+            plurielArticleBoutonPanier = "article";
+        }
 
-        affichageCoteBoutonPanier = '<button><a href="index.html">Accueil / Catalogue</a></button><button><a href="panier.html" title="'+prixTotalDuPanier+'€">Panier ('+totalDesArticlesDuPanier+' '+plurielArticleBoutonPanier+')</a></button>'; 
-         }
+        affichageCoteBoutonPanier = '<button><a href="index.html">Accueil / Catalogue</a></button><button><a href="panier.html" title="' + prixTotalDuPanier + '€">Panier (' + totalDesArticlesDuPanier + ' ' + plurielArticleBoutonPanier + ')</a></button>';
+    }
 
     return affichageCoteBoutonPanier;
 }
-     
- //////////////////////////////////////////////  END    ////////////////////////////////////////////////////
- 
+
+//////////////////////////////////////////////  END    ////////////////////////////////////////////////////
+
 ///////////////////////////////   AFFICHAGE EN EURO   ///////////////////////////////////////////
 
 
@@ -72,48 +72,48 @@ let affichageCoteBoutonPanier = null;
 
 
 //////////////////////////////////////// MESSAGE FLASH UTILISATEUR  //////////////////////////////////////////////////
-var affichageMessageFlash = function(){
-   let message = localStorage.messagePanier;
-        if(message != "vide"){
-        let affichageMessageFlash = '<div id="textFlashMessage">'+message+'</div>';
-        document.getElementById("flashMessage").innerHTML = '<div id="textFlashMessage">'+message+'</div>';
-        }
+var affichageMessageFlash = function () {
+    let message = localStorage.messagePanier;
+    if (message != "vide") {
+        let affichageMessageFlash = '<div id="textFlashMessage">' + message + '</div>';
+        document.getElementById("flashMessage").innerHTML = '<div id="textFlashMessage">' + message + '</div>';
+    }
     localStorage.messagePanier = "vide"; // on remet vide en cas de rechargement de la page
 }
 
 //////////////////////////////////////////// SUPPRIMER UN PRODUIT ////////////////////////////////////////////
-let fonctionDelete = function(a){ // supprimer un produit du panier
-var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local
-    if(data.length == 1){// on va supprimer le dernier produit
+let fonctionDelete = function (a) { // supprimer un produit du panier
+    var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local
+    if (data.length == 1) { // on va supprimer le dernier produit
         localStorage.removeItem("panier");
         localStorage.setItem("panier", "vide");
         localStorage.setItem("messagePanier", "Panier vidé !")
-        window.location.href = "panier.html";// on revient à la page d'acceuil */ 
-    }else{
+        window.location.href = "panier.html"; // on revient à la page d'acceuil */ 
+    } else {
         localStorage.messagePanier = "Produit supprimé !"; // message utilisateur
-        data.splice(a,1); // on supprime l'objet correspondant
-        localStorage.setItem("panier", JSON.stringify(data));// Sauvegarde du panier mis à jour
-        window.location.href = "panier.html";// on revient à la page d'acceuil */ 
+        data.splice(a, 1); // on supprime l'objet correspondant
+        localStorage.setItem("panier", JSON.stringify(data)); // Sauvegarde du panier mis à jour
+        window.location.href = "panier.html"; // on revient à la page d'acceuil */ 
     }
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////// MODIFICATION QUANTITE ////////////////////////////////////////////
 
-var fonctionActionPanier = function(a,Number){
-var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
-var produitTrouve = false;
-            for(let x in data) {
-                 if(data[x].reference == a){
-                     produitTrouve = true;
-                     // Augmenter la quantité et le prix
-                     data[x].quantite = data[x].quantite + Number;
-                     data[x].prixAjour = data[x].quantite * data[x].prixUnitaire;
-                 }
-            }
-            // Sauvegarde du panier mis à jour
-            localStorage.setItem("panier", JSON.stringify(data));
-            window.location.href = "panier.html";// on revient à la page du panier    
+var fonctionActionPanier = function (a, Number) {
+    var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
+    var produitTrouve = false;
+    for (let x in data) {
+        if (data[x].reference == a) {
+            produitTrouve = true;
+            // Augmenter la quantité et le prix
+            data[x].quantite = data[x].quantite + Number;
+            data[x].prixAjour = data[x].quantite * data[x].prixUnitaire;
+        }
+    }
+    // Sauvegarde du panier mis à jour
+    localStorage.setItem("panier", JSON.stringify(data));
+    window.location.href = "panier.html"; // on revient à la page du panier    
 }
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 
