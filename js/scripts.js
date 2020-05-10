8f923b6bce463e3a73debc5f0736654c1e7a2c79
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-05-10 17:00:55 +0200

    ok steph

--- js/scripts.js
+++ js/scripts.js
@@ -102,21 +102,31 @@ let fonctionDelete = function (a) { // supprimer un produit du panier
 
 //////////////////////////// MODIFICATION QUANTITE PLUS////////////////////////////////////////////
 
-var fonctionQuantitePlus = function (a) {
+var fonctionQuantitePlus = function (reference) {
     var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local
+    var produitTrouve = false;
+            for (let x in data) {
+                if (data[x].reference == reference) {
+                    produitTrouve = true;
 
-    // Sauvegarde du panier mis à jour
-    //localStorage.setItem("panier", JSON.stringify(data));
-    //window.location.href = "panier.html"; // on revient à la page du panier    
+                    // Augmenter la quantité et le prix
+                    data[x].quantite++;
+                    data[x].prixAjour = data[x].quantite * data[x].prixUnitaire;
+                }
+            }
+            localStorage.messagePanier = "Quantité mise à jour !";
+            // Sauvegarde du panier mis à jour
+            localStorage.setItem("panier", JSON.stringify(data));
+            window.location.href = "panier.html";
 }
 ////////////////////////////////////////// END ///////////////////////////////////////////////
 //////////////////////////// MODIFICATION QUANTITE MOINS////////////////////////////////////////////
 
-var fonctionQuantiteMoins = function (a) {
+var fonctionQuantiteMoins = function (reference) {
     var data = JSON.parse(localStorage.getItem("panier")); // on recupere le panier en local 
     var produitTrouve = false;
     for (let x in data) {
-        if (data[x] == a) {
+        if (data[x].reference == reference) {
             produitTrouve = true;
             // Augmenter la quantité et le prix
             data[x].quantite--;
@@ -135,5 +145,70 @@ var fonctionClearPanier = function () {
     localStorage.messagePanier = "Panier vidé !";
     window.location.href = "index.html"; //et on va page accueil
 }
+
+
+////////////////////////////////////////// END ///////////////////////////////////////////////
+//////////////////////////// ENVOIE DE LA COMMANDE ////////////////////////////////////////////
+
+var fonctionSubmitContact = function () {
+    const prenom = document.getElementById('prenom').value; //recupere prenom
+    const nom = document.getElementById('nom').value; //recupere catalogue 
+    const mail = document.getElementById('email').value; //recupere catalogue
+    const ville = document.getElementById('ville').value; //recupere catalogue  
+    const adresse = document.getElementById('adresse').value; //recupere catalogue  
+    const codePostal = document.getElementById('codePostal').value; //recupere catalogue
+
+    /// format pour l'envoie
+    class formatToSend {
+        constructor(utilisateur, idSacommander) {
+            this.contact = utilisateur;
+            this.products = idSacommander;
+        }
+    }
+    // format pour l'utilisateur
+    class formatUtilisateur {
+        constructor(nom, prenom, adresse, ville, mail) {
+            this.firstName = prenom;
+            this.lastName = nom;
+            this.address = adresse;
+            this.city = ville;
+            this.email = mail;
+        }
+    }
+
+    // construction de l'utilisateur
+    const utilisateur = new formatUtilisateur(prenom, nom, adresse, ville, mail);
+
+    
+    // construction de l'array avec uniquement les identifiants des produits
+    var paniersLocal = JSON.parse(localStorage.getItem("panier")); // on recupere le panier
+    var idSacommander = [];
+    for (var x = 0; x < paniersLocal.length; x++) {
+        var ligneProduit = paniersLocal[x];
+        idSacommander.push(ligneProduit.reference);
+    }
+
+    // on a tout donc on forme la commande prete a envoyer
+    const commandeToSend = new formatToSend(utilisateur, idSacommander)
+
+    // on envoie
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 201) {
+            var response = JSON.parse(this.responseText);
+            localStorage.messagePanier = "MERCI";
+            localStorage.panier = "vide";
+            localStorage.setItem("confirmCommande", response.orderId);
+            window.location.href = "confirmation.html"; // on va de tout facon à la page de confirmation
+        }
+    }; // fin de la fonction
+    xhttp.open("POST", "http://localhost:3000/api/teddies/order");
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(JSON.stringify(commandeToSend));
+        
+    return false;
+}
+////////////////////////////////////////// END ///////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////// END GESTION DU PANIER /////////////////////////////////////////////////////////////////////
